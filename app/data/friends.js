d4dd83cafdbf26e5fbed3a15ef5bc1a379c05cd5
ncloyd <ncloyd33@gmail.com>
2017-12-13 09:13:21 -0600

    post to Friends API list is working

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,13 +4,13 @@
 // Initially we just set it equal to a "dummy" friend.
 // ===============================================================================
 
-var friendsArray = [
+var friends = [
   {
    	"name": "Ahmed",
     "photo": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
-    "scores": [1, 3, 2, 4, 3, 5, 2, 4, 2, 1]
+    "scores": [3, 3, 2, 4, 3, 2, 2, 4, 2, 4]
   },
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = friendsArray;
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,91 +4,51 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friends = require("../data/friends");
-
-
-
+var friends = require("../data/friends.js");
+console.log(friends)
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
     // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-
     app.get("/api/friends", function(req, res) {
-        res.json(friendsArray);
+        res.json(friends);
     });
-
     // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-
     app.post("/api/friends", function(req, res) {
+    
+      // Here we take the result of the user's survey POST and parse it.
+        var newFriend = req.body;
+        var userScores = newFriend.scores;
+        
+        var matchName = '';
+        var matchPhoto = '';
+        var totalDifference = 10000;
 
-
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
-
-        // Here we take the result of the user's survey POST and parse it.
-        var userData = req.body;
-        var userName = userData.name;
-        var userPhoto = userData.photo;
-        var userScores = userData.scores;
-
-        var totalDifference = 0;
-
+        console.log(friends)
+        console.log(newFriend);
         // Loop through all the friend possibilities in the database. 
-        for (var i = 0; i < friendsArray.length; i++) {
 
-            console.log(friendsArray[i].name);
-            totalDifference = 0;
-
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+            
             // Loop through all the scores of each friend
-            for (var j = 0; j < friendsArray[i].scores[j]; j++) {
+            for (var j = 0; j < friends[i].scores[j]; j++) {
 
                 // We calculate the difference between the scores and sum them into the totalDifference
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                // diff += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
 
                 // If the sum of differences is less then the differences of the current "best match"
-                if (totalDifference <= bestMatch.friendDifference) {
+                if (diff < totalDifference) {
 
-                    // Reset the bestMatch to be the new friend. 
-                    bestMatch.name = friendsArray[i].name;
-                    bestMatch.photo = friendsArray[i].photo;
-                    bestMatch.friendDifference = totalDifference;
+                    totalDifference = diff;
+                    matchName = friends[i].name;
+                    matchImage = friends[i].photo;
                 }
             }
         }
-
-        // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-        // the database will always return that the user is the user's best friend).
-        friends.push(userData);
-
-        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
-        res.json(bestMatch);
-
+        friends.push(newFriend);
+        res.json({status: 'OK', matchName: matchName, matchPhoto: matchPhoto});
     });
 };
-
-// ---------------------------------------------------------------------------
-// I added this below code so you could clear out the table while working with the functionality.
-// Don"t worry about it!
-
-// app.post("/api/clear", function() {
-//   // Empty out the arrays of data
-//   tableData = [];
-//   waitListData = [];
-
-//   console.log(tableData);
-// });
